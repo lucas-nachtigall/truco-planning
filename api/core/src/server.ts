dfa86ce506a11b3844908a4da47121a4789e7408
Rodrigo Polydoro <rspolydoro@gmail.com>
2023-06-26 11:02:04 -0300

    Testing cors

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -11,7 +11,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(cors({
-  origin: '*'
+  origin: ['https://trucoplanning.bohr.io', 'http://localhost:5173']
 }));
 
 app.use('/', routes)
